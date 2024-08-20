b0667bdd70e6a39a953fa6d28b2f18d034ada917
Mustafa Ozyurt <mustafa.ozyurt@gmail.com>
2024-08-20 22:57:13 +0300

    s3 image check

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/joho/godotenv"
 	"log"
 	"net/http"
@@ -26,5 +32,43 @@ func main() {
 }
 
 func checkImageHandler(w http.ResponseWriter, r *http.Request) {
+	playerId := r.URL.Query().Get("playerId")
+	if playerId == "" {
+		http.Error(w, "playerId is required", http.StatusBadRequest)
+		return
+	}
 
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String(os.Getenv("AWS_REGION")),
+	})
+	if err != nil {
+		http.Error(w, fmt.Sprintf("failed to create session: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	svc := s3.New(sess)
+	bucket := os.Getenv("S3_BUCKET")
+	key := fmt.Sprintf("players/%s.png", playerId)
+
+	_, err = svc.HeadObject(&s3.HeadObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+	})
+
+	response := map[string]string{"imgUrl": ""}
+	if err != nil {
+		var awsErr awserr.Error
+		if errors.As(err, &awsErr) && awsErr.Code() == s3.ErrCodeNoSuchKey {
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(response)
+			return
+		}
+		http.Error(w, fmt.Sprintf("failed to check if object exists: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	imgUrl := fmt.Sprintf("https://s3.%s.amazonaws.com/%s/%s", os.Getenv("AWS_REGION"), bucket, key)
+	response["imgUrl"] = imgUrl
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/fmo/s3-api
 
 go 1.22
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require (
+	github.com/aws/aws-sdk-go v1.55.5 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
